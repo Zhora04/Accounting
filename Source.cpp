763e818de58014ab399e1e56a5a7edd20691aeed
Zhora04 <76618324+Zhora04@users.noreply.github.com>
2020-12-29 17:10:51 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <cstdio>
+#include <climits>
+#include <tuple>
+#include <ctime>
+#include <cstring>
+#include <numeric>
+#include <functional>
+#include <chrono>
+#include <assert.h>
+using ll = long long;
+using namespace std;
+const ll mod = 1e9 + 7;
+const int N = 1e6 + 5;
+int arr[N], last[N];
+
+
+void test_case()
+{
+    int n, q;
+    scanf("%d%d", &n, &q);
+    int restart_time = -1, now = -1;
+    char s[8];
+    for (int i = 1; i <= q; i++)
+    {
+        scanf("%s", s);
+        if (s[0] == 'P')
+        {
+            int x;
+            scanf("%d", &x);
+            x--;
+            if (last[x] > restart_time) cout << arr[x] << "\n";
+            else cout << now << "\n";
+        }
+        else if (s[0] == 'R')
+        {
+            int x;
+            scanf("%d", &x);
+            restart_time = i;
+            now = x;
+        }
+        else
+        {
+            int x, y;
+            scanf("%d%d", &x, &y);
+            x--;
+            last[x] = i;
+            arr[x] = y;
+        }
+    }
+}
+
+int main()
+{
+    /*int tc;
+    cin >> tc;
+    while (tc--)*/
+        test_case();
+
+    return 0;
+}
